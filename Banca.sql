3484f5763f6b44dfe53e6388f7a0235010eef773
Eduardo Catalán <marvineduardocv12@gmail.com>
2020-12-23 09:47:03 -0600

    cambios en la DB

--- Banca.sql
+++ Banca.sql
@@ -6,30 +6,50 @@ Nombre varchar(15) not null,
 contra varchar(20) not null,
 primary key(Nombre)
 );
-insert into ADMINISTRADOR(Nombre,contra) values('administrador','admin');
-
+insert into ADMINISTRADOR(Nombre,contra) values('admin','admin');
+select * from clienteindividual;
+create table clienteIndividual(
+usuarioI varchar(20) primary key,
+pass varchar(20),
+cui bigint(20),
+nit int(11),
+nombre varchar(20),
+apellido varchar(20),
+fehcanac date,
+telefono int(9)
+);
 create table USUARIOINDIVIDUAL(
-CUI int(13) unsigned zerofill primary key,
-NIT int(7) unsigned zerofill default null,
-nombres varchar(35) not null,
-apellidos varchar(35) not null,
-fechaNacimiento date not null
+CUI bigint(20) primary key,
+NIT int(11),
+nombres varchar(30),
+apellidos varchar(30),
+fechaNacimiento date
 );
 
 create table USUARIOEMPRESARIAL(
 idUsuarioemp int auto_increment primary key,
 tipoEmpresa enum('sociedad anonima','compañia limitada','empresa individual'),
-nombre varchar(40) not null,
-nombreComercial varchar(40) not null,
-nombresRepresentante varchar(35) not null,
-apellidosRepresentante varchar(35) not null
+nombre varchar(40),
+nombreComercial varchar(40),
+nombresRepresentante varchar(35),
+apellidosRepresentante varchar(35)
 );
 create table USUARIO(
 idUsuario int auto_increment primary key,
-idTipoUsuario int not null,
-CUI int(13) unsigned zerofill,
+contra varchar(20),
+CUI bigint(20),
 idUsuarioemp int,
 foreign key (CUI) references USUARIOINDIVIDUAL(CUI),
 foreign key (idUsuarioemp) references USUARIOEMPRESARIAL(idUsuarioemp)
 );
+
+/*CAMPO DE PRUEBAS*/
+insert into usuarioindividual values(12345,124,'loca ersr','erdsr fer','1985-08-12');
+select * from usuarioindividual;
+delete from usuarioindividual where cui=12345;
+insert into usuario(contra,cui) values('123',123);
+select * from usuario;
+select * from usuarioindividual;
+drop table usuarioempresarial;
+drop table usuarioindividual;
 
